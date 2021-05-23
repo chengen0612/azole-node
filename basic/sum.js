37aa3e54feb75e94624fed7b9f8eacf03471df29
ashleylai <ashleylai58@gmail.com>
2021-05-23 08:55:43 +0800

    完成 sum.js

--- basic/sum.js
+++ basic/sum.js
@@ -1,8 +1,22 @@
 function sum(n) {
-  // TODO
+  // O(1)
+  // return ((1 + n) * n) / 2;
+
+  // O(n)
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
 }
 
 console.log(sum(1)); // 1
 console.log(sum(2)); // 3
 console.log(sum(10)); // 55
 console.log(sum(100000)); // 5000050000
+
+console.time("SUM");
+for (let i = 0; i <= 10000; i++) {
+  sum(100000);
+}
+console.timeEnd("SUM");
