5945a25b74679c45d9e760e6dcb4cbaa3b2808f2
ashleylai <ashleylai58@gmail.com>
2021-05-23 14:24:08 +0800

    add comment for O

--- basic/sum.js
+++ basic/sum.js
@@ -1,19 +1,24 @@
 function sum(n) {
-  // O(1)
+  // O(1) 常數
   // return ((1 + n) * n) / 2;
 
-  // O(n)
+  // O(n) 變數 -> 時間上複雜度會隨著 n 而成長
   let sum = 0;
   for (let i = 1; i <= n; i++) {
-    sum += i;
+    sum += 1;
   }
   return sum;
 }
 
-console.log(sum(1)); // 1
-console.log(sum(2)); // 3
-console.log(sum(10)); // 55
+// console.log(sum(1)); // 1
+// console.log(sum(2)); // 3
+// console.log(sum(10)); // 55
 console.log(sum(100000)); // 5000050000
+console.time("SUM1");
+for (let i = 0; i <= 10000; i++) {
+  sum(2);
+}
+console.timeEnd("SUM1");
 
 console.time("SUM");
 for (let i = 0; i <= 10000; i++) {
