b5d9262071b006ce9409c4f02b9aaffbc095c1b6
ashleylai <ashleylai58@gmail.com>
2021-06-06 14:31:12 +0800

    using dotenv in database example

--- database/await.js
+++ database/await.js
@@ -1,13 +1,12 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "tester",
-  password: "tester",
-  database: "stock",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
-
 function connect() {
   return new Promise((resolve, reject) => {
     connection.connect(function (err) {
--- database/bluebird.js
+++ database/bluebird.js
@@ -1,12 +1,12 @@
 const bluebird = require("bluebird");
 const mysql = require("mysql");
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "tester",
-  password: "tester",
-  database: "stock",
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection = bluebird.promisifyAll(connection);
