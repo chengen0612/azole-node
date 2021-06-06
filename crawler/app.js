b4a8ce4c1c5bad2698e2a5d480610b131b133484
ashleylai <ashleylai58@gmail.com>
2021-06-06 10:37:27 +0800

    add crawler app and using dotenv

--- crawler/app.js
+++ crawler/app.js
@@ -6,30 +6,52 @@
 // npm i axios
 // 引入 axios
 const axios = require("axios");
+const moment = require("moment");
+const fs = require("fs/promises");
+const mysql = require("mysql");
+const Promise = require("bluebird");
+require("dotenv").config();
 
-// TODO: 從 stock.txt 讀股票代碼進來
-// filesystem
-// npm i fs ??? -> 不用
-const fs = require("fs");
-
-fs.readFile("stock.txt", "utf8", (err, data) => {
-  if (err) {
-    return console.error("讀檔錯誤", err);
-  }
-  console.log(`讀到的 stock code: ${data}`);
-
-  axios
-    .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-      params: {
-        response: "json",
-        date: "20210523",
-        stockNo: data,
-      },
-    })
-    .then(function (response) {
-      if (response.data.stat === "OK") {
-        console.log(response.data.date);
-        console.log(response.data.title);
-      }
-    });
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
+connection = Promise.promisifyAll(connection);
+
+(async function () {
+  try {
+    await connection.connectAsync();
+
+    let stockCode = await fs.readFile("stock.txt", "utf8");
+    console.log(`讀到的 stock code: ${stockCode}`);
+    let stock = await connection.queryAsync(
+      `SELECT stock_id FROM stock WHERE stock_id = ${stockCode}`
+    );
+
+    if (stock.length <= 0) {
+      console.log("Start to query name");
+      let response = await axios.get(
+        `https://www.twse.com.tw/zh/api/codeQuery?query=${stockCode}`
+      );
+      let answer = response.data.suggestions.shift();
+      let answers = answer.split("\t");
+      console.log(answers);
+      if (answers.length > 1) {
+        console.log(`儲存股票名稱 ${answers[0]} ${answers[1]}`);
+        connection.queryAsync(
+          `INSERT INTO stock (stock_id, stock_name) VALUES ('${answers[0]}', '${answers[1]}');`
+        );
+      } else {
+        throw "查詢股票名稱錯誤";
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    connection.end();
+  }
+})();
